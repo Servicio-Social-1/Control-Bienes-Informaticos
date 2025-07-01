2bd120401b6b65bd726bade4a97b96d5949d7127
Moises Israel Marin Mora <moises.mora@dap.local>
2025-06-30 18:33:23 -0600

    botones

--- Control-Bienes/src/app/app.routes.ts
+++ Control-Bienes/src/app/app.routes.ts
@@ -1,6 +1,24 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './inicio/inicio';
+import { Registro } from './registro/registro';
+import { DAPSIS } from './dap-sis/dap-sis';
+import { IPyDirFisica } from './ipy-dir-fisica/ipy-dir-fisica';
+import { EquiposSinIP } from './equipos-sin-ip/equipos-sin-ip';
+import { IpDisponible } from './ip-disponible/ip-disponible';
+import { Area } from './area/area';
+import { AreaEspecifica } from './area-especifica/area-especifica';
+import { Responsable } from './responsable/responsable';
+import { ResponsableEspecifico } from './responsable-especifico/responsable-especifico';
 
 export const routes: Routes = [
     {path: '', component: Inicio},
+    {path: 'registro', component: Registro},
+    {path: 'DAP-SIS', component: DAPSIS},
+    {path: 'IPyDirFisica', component: IPyDirFisica},
+    {path: 'equipos-sin-IP', component: EquiposSinIP},
+    {path: 'IP-Disponible', component: IpDisponible},
+    {path: 'area', component: Area},
+    {path: 'area-Especifica', component: AreaEspecifica},
+    {path: 'responsable', component: Responsable},
+    {path: 'responsable-Especifico', component: ResponsableEspecifico},
 ];
--- Control-Bienes/src/app/inicio/inicio.ts
+++ Control-Bienes/src/app/inicio/inicio.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { Registro } from '../registro/registro';
+import { RouterModule, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css'
 })
 export class Inicio {
-
+  constructor(private router: Router) {}
 }
